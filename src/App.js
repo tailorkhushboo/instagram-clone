03f30a7ec0f4fb50729328fe69f3cf1795636b78
tailorkhushboo <khushbootailor111@gmail.com>
2020-10-16 09:44:44 -0700

    creating static post layout(mutiple)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Post from "./Post";
+import react_logo from './react-logo.jpg';
+
 function App() {
   return (
     <div className="app">
@@ -9,11 +11,13 @@ function App() {
           <img
             className="app__headerImage"
             src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
-            alt=""
           />
         </div>
         <h1>Header</h1>
-        <Post />
+        <Post username="Khushboo" caption="One click import demo content which includes post, pages, comments etc. " imageUrl={react_logo}/>
+        <Post username="Neel" caption=" import demo content for different" imageUrl="https://www.gstatic.com/webp/gallery3/2.png"/>
+        <Post username="Jay" caption="Click on Get Started" imageUrl="https://www.gstatic.com/webp/gallery3/4_webp_ll.png"/>
+        <Post username="Dhruvin" caption="Click on Start Import." imageUrl="https://www.gstatic.com/webp/gallery3/3_webp_ll.png"/>
         {/* Posts */}
     </div>
   );
--- src/Post.js
+++ src/Post.js
@@ -1,20 +1,29 @@
 import React from "react";
 import react_logo from './react-logo.jpg';
-
-function Post(){
+import './Post.css';
+import Avatar from "@material-ui/core/Avatar";
+function Post({ username, caption, imageUrl }){
     return(
         <div className="post">
-            <h3>Username</h3>
+            <div className="post__header">
+                <Avatar
+                    className="post__avatar"
+                    alt='images'
+                    src="/static/images/avatar/1.jpg"
+                />
+                <h3>{username}</h3>
+            </div>
+
 
             {/* header -> avatar + username */}
             <img
                 className="post__image"
-                // src={require("./react-logo.jpg")}
-                src={react_logo}
+                src={imageUrl}
             />
 
             {/* image */}
-            <p className="post__text">This website example <strong>http://www.cruiseshipodyssey.com/worlds-fleet/</strong> I found doesn’t use react, but shows ships based on specific cruise.</p>
+            <h4 className="post__text"><strong> {username}</strong> {caption}</h4>
+            {/*<strong className="post__text">{username} </strong>{caption}*/}
             {/* username + caption */}
         </div>
     );
